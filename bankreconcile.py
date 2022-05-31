6dc85f0c4c2238d3243fd4801830d7a94ca38bb3
uniz <nguyen_uniz@yahoo.ca>
2022-05-30 23:36:37 -0500

    clean up some functions

--- bankreconcile.py
+++ bankreconcile.py
@@ -11,13 +11,13 @@ BankStatementPath = os.path.join(BASE_DIR,'BankStatement.xlsx')
 OutputExcelPath = os.path.join(BASE_DIR,'Reconciliation.xlsx')
 
 #DateFrom and DateTo paramters for the query 
-DateFrom = "{d'2019-01-01'}"
-DateTo = "{d'2019-10-23'}"
-
+DateFrom = "{d'2021-01-01'}"
+DateTo = "{d'2022-05-31'}"
 # open Excel file from bank statement, create dataframe from worksheet
 df = pd.read_excel(BankStatementPath, header=0, dtype={'Reference':str})
 df['Debit Amount'] = df['Debit Amount'].replace(np.nan,0)
 df['Credit Amount'] = df['Credit Amount'].replace(np.nan,0)
+df['Date'] = df['Date'].dt.date
 
 def getpendingcheckno(row):
     if "Pending Check" in row['Memo']:
@@ -49,35 +49,21 @@ Debit = Debit.merge(Check,how = 'left', indicator = True)
 OtherDebit = Debit[Debit['_merge'] == 'left_only']
 OtherDebit.drop(['_merge'],axis = 1, inplace = True)
 
-list1 = []
-counter1 = []
 
-for index, row in OtherDebit.iterrows():
-    list1.append(row['Debit'])
-    counter1.append(list1.count(row['Debit']))
+OtherDebit['Counter'] = OtherDebit.groupby(['Debit']).cumcount().add(1)
+
+OtherDebit['Combine'] = OtherDebit['Debit'].astype(str) + '|' + OtherDebit['Counter'].astype(str)
 
 
-OtherDebit.loc[:,'Counter'] = counter1
+Credit['Counter'] = Credit.groupby(['Credit']).cumcount().add(1)
 
 
-OtherDebit.loc[:,'Combine'] = OtherDebit['Debit'].astype(str) + '|' + OtherDebit['Counter'].astype(str)
-
-
-
-list3 = []
-counter3 = []
-
-for index, row in Credit.iterrows():
-    list3.append(row['Credit'])
-    counter3.append(list3.count(row['Credit']))
-
-Credit.loc[:,'Counter'] = counter3
-Credit.loc[:,'Combine'] = Credit['Credit'].astype(str) + '|' + Credit['Counter'].astype(str)
+Credit['Combine'] = Credit['Credit'].astype(str) + '|' + Credit['Counter'].astype(str)
 
 
 
 # create a new comlum to combine transaction amount and check number
-Check.loc[:,'Combine'] = Check['Debit'].astype(str) + '|' + Check['Reference'].astype(str)
+Check['Combine']= Check['Debit'].astype(str) + '|' + Check['Reference'].astype(str)
 
 
 
@@ -98,7 +84,7 @@ df2['Credit'] = df2['Credit'].replace(np.nan,0)
 df2['RefNumber'] = df2['RefNumber'].replace(np.nan,0)
 df2.rename(columns = {'Debit':'Credit','Credit':'Debit'}, inplace = True)
 
-
+df2['Date'] = df2['Date'].dt.date
 df2.drop(['ClearedStatus',], axis=1,inplace=True)
 
 # remove rows that have transaction amount = 0
@@ -120,48 +106,34 @@ Check2 = Debit2[Debit2['RefNumber'].str.contains('^\d{5}$',regex = True, na=Fals
 Check2 = Check2[Check2['TxnType'] != 'Invoice']
 
 #combine Check amount with Check Number
-Check2.loc[:,'Combine'] = Check2['Debit'].astype(str) + '|' + Check2['RefNumber'].astype(str)
+Check2['Combine'] = Check2['Debit'].astype(str) + '|' + Check2['RefNumber'].astype(str)
 
 Debit2 = Debit2.merge(Check2,how = 'left', indicator = True)
 OtherDebit2 = Debit2[Debit2['_merge'] == 'left_only']
 OtherDebit2.drop(['_merge'],axis = 1, inplace = True)
 
-list2 = []
-counter2 = []
-
-for index, row in OtherDebit2.iterrows():
-    list2.append(row['Debit'])
-    counter2.append(list2.count(row['Debit']))
+OtherDebit2['Counter'] = OtherDebit2.groupby(['Debit']).cumcount().add(1)
 
 
-OtherDebit2.loc[:,'Counter'] = counter2
+OtherDebit2['Combine'] = OtherDebit2['Debit'].astype(str) + '|' + OtherDebit2['Counter'].astype(str)
 
-
-OtherDebit2.loc[:,'Combine'] = OtherDebit2['Debit'].astype(str) + '|' + OtherDebit2['Counter'].astype(str)
-
-
-list4 = []
-counter4 = []
-
-for index, row in Credit2.iterrows():
-    list4.append(row['Credit'])
-    counter4.append(list4.count(row['Credit']))
-
-Credit2.loc[:,'Counter'] = counter4
-Credit2.loc[:,'Combine'] = Credit2['Credit'].astype(str) + '|' + Credit2['Counter'].astype(str)
+Credit2['Counter'] = Credit2.groupby(['Credit']).cumcount().add(1)
+Credit2['Combine'] = Credit2['Credit'].astype(str) + '|' + Credit2['Counter'].astype(str)
 
 
 
-Check.loc[:,'Matched'] = Check['Combine'].isin(Check2['Combine'])
-Check2.loc[:,'Matched'] = Check2['Combine'].isin(Check['Combine'])
+
+Check['Matched'] = Check['Combine'].isin(Check2['Combine'])
+Check2['Matched'] = Check2['Combine'].isin(Check['Combine'])
 
 
-OtherDebit.loc[:,'Matched'] = OtherDebit['Combine'].isin(OtherDebit2['Combine'])
-OtherDebit2.loc[:,'Matched'] = OtherDebit2['Combine'].isin(OtherDebit['Combine'])
+OtherDebit['Matched'] = OtherDebit['Combine'].isin(OtherDebit2['Combine'])
+OtherDebit2['Matched'] = OtherDebit2['Combine'].isin(OtherDebit['Combine'])
 
 
-Credit.loc[:,'Matched'] = Credit['Combine'].isin(Credit2['Combine'])
-Credit2.loc[:,'Matched'] = Credit2['Combine'].isin(Credit['Combine'])
+
+Credit['Matched'] = Credit['Combine'].isin(Credit2['Combine'])
+Credit2['Matched'] = Credit2['Combine'].isin(Credit['Combine'])
 
 
 writer = pd.ExcelWriter(OutputExcelPath,engine='xlsxwriter')
